303284082a8eb6214dc46bf300593bfdda6a1dbb
Eric Yan <yifeiyan@umich.edu>
2025-01-13 22:53:42 -0500

    module 4: Add Dynamic Pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,6 @@
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
@@ -7,6 +10,9 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path='/register' element={<Register />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
